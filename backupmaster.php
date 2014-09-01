ad07be1e0bff6d8ee1a3ac4802fd54d4bb7f5259
Lesyuk Serg <seranatles@gmail.com>
2014-09-01 14:59:43 +0300

    beauty fix

--- backupmaster.php
+++ backupmaster.php
@@ -1,59 +1,58 @@
 <?php
-    set_time_limit(0); // Убираем ограничение на максимальное время работы скрипта
+    # При запуску цього файлу:
+    #   1. Будуть видалені всі архіви котрі були створені більше ніж %delay_delete% днів назад(в директорії росташування файлу)
+    #   2. Будуть вибрані всі бази mysql на сервері localhost. I будуть створені їх дампи в форматі SQL
+    #   3. Всі створені дампи будуть запаковані в ZIP архів(zip архів буде росташовано в директорії росташування файлу)
+    
+    set_time_limit(0);
     date_default_timezone_set('Europe/Kiev');
     
-    //$dump_dir = getcwd(); // Директория, куда будут помещаться архивы
-    $dump_dir = dirname(__FILE__); // Директория, куда будут помещаться архивы
-    
-    $delay_delete = 90 * 24 * 3600; // Время в секундах, через которое архивы будут удаляться
-    $filezip = 'backup_'.date("Y-m-d_His").'.zip'; // Имя архива
     $timestart = microtime(1);
-    $db_names = array();
+    //$dump_dir = getcwd(); # Директорія, для розміщення файлів
+    $dump_dir = dirname(__FILE__); # Директорія, для розміщення файлів
     
-    /* Параметры подключения к базе данных */
-    $connect = array(
-        'host' => 'localhost',
-        'user' => 'root',
-        'pass' => '',
-    );
+    $delay_delete = 90 * 24 * 3600; # Через скільки днів будуть видалені архіви (90 днів)
+    $filezip = 'backup_'.date("Y-m-d_His").'.zip'; # Ім'я архіву
     
-    deleteOldArchive($dump_dir, $delay_delete); // Удаляем все старые архивы
+    # Параметри підключення до бази данних
+    $connect = array('host' => 'localhost', 'user' => 'root', 'pass' => '');
     
-    $db_names = get_databases($connect); // Получаем все базы данных
+    deleteOldArchive($dump_dir, $delay_delete); # Видаляєм всі старі архіви
+    
+    $db_names = get_databases($connect); # Отримаємо сптсок всіх баз данних
     
     start($dump_dir, $filezip, $connect, $db_names);
     
-    echo 'выполнение скрипта: '.round(microtime(1)-$timestart, 4).' c<br />';
-    
+    echo 'Робота скрипта: '.round(microtime(1)-$timestart, 4).' c<br />';
     
     
 # ---ФУНКЦІЇ--------------------------------------------------------------------
-    function get_databases($cnct)
-    { // Получаем все базы
+    function get_databases($cnct) # Отримаємо сптсок всіх баз данних
+    { 
         $db_names = array();
         
-        $db = new mysqli($cnct['host'], $cnct['user'], $cnct['pass']); // Соединяемся с базой данных
-        $db->query("SET NAMES 'utf-8'"); // Устанавливаем кодировку соединения
+        $db = new mysqli($cnct['host'], $cnct['user'], $cnct['pass']); # З'єднуємося з базою даних
+        $db->query("SET NAMES 'utf-8'"); # Встановлюємо кодування
         $result_set = $db->query('SHOW DATABASES');
         
-        echo 'базы которые будут обработаны: ';
+        echo 'Бази котрі будуть оброблені: ';
         while ($dbase = $result_set->fetch_row()) {
             $db_names[] = $dbase[0];
             echo $dbase[0].', ';
         }
-        $db->close(); // Закрываем соединение с базой данных и переходим к следующей
+        $db->close(); # Закриваємо з'єднання з базою даних
         echo '<br />';
         
         return $db_names;
     }
     
-    function deleteOldArchive($dump_dir, $delay_delete) // Удаляем все старые архивы
+    function deleteOldArchive($dump_dir, $delay_delete) # Видаляєм всі старі архіви
     {
         $ts = time();
         $files = glob($dump_dir."/*.zip");
         foreach ($files as $file) {
             if ($ts - filemtime($file) > $delay_delete) {
-                echo 'Удален старый дамп: '.$file.'<br />';
+                echo 'Видалено старий дамп: '.$file.'<br />';
                 unlink($file);
             }
         }
@@ -61,13 +60,13 @@
     
     function start($dump_dir, $filezip, $connect, $db_names)
     {
-        echo 'создание дампа: '.$filezip.'<br />';
+        echo 'створення дампу: '.$filezip.'<br />';
         
-        if (file_exists($dump_dir."/".$filezip)) exit; // Если архив с таким именем уже есть, то заканчиваем скрипт
+        if (file_exists($dump_dir."/".$filezip)) exit; # Якщо такий архів вже є то вихід
         
-        $db_files = array(); // Массив, куда будут помещаться файлы с дампом баз данных
+        $db_files = array(); # Масив імен баз данных
         foreach ($db_names as $name) {
-            $db_files[] = $dump_dir.'/'.$name.'.sql'; // Помещаем файл в массив
+            $db_files[] = $dump_dir.'/'.$name.'.sql';
             db_dump($connect, $dump_dir, $name);
         }
         
@@ -76,32 +75,32 @@
         
         foreach ($db_files as $file) {
             unlink($file);
-            echo '<b><font color="green">УДАЛЕН</font></b> архив <b>'.$file.'</b><br />';
+            echo '<b><font color="green">ВИДАЛЕНИЙ</font></b> архів <b>'.$file.'</b><br />';
         }
     }
     
     function db_dump($cnct, $dump_dir, $db_name)
     {
         passthru('mysqldump --host='.$cnct['host'].' --user='.$cnct['user'].' --password='.$cnct['pass'].' '.$db_name.' > '.$dump_dir.'/'.$db_name.'.sql');
-        echo '<b><font color="green">СОЗДАН</font></b> дамп базы <b>'.$db_name.'</b><br />';
+        echo '<b><font color="green">СТВОРЕНО</font></b> дамп бази <b>'.$db_name.'</b><br />';
         
         flush();
     }
     
     function archivation($dump_dir, $filezip, $db_files)
     {
-        $zip = new ZipArchive(); // Создаём объект класса ZipArchive
+        $zip = new ZipArchive();
         if ($zip->open($dump_dir."/".$filezip, ZipArchive::CREATE) === true) {
-            /* Добавляем в ZIP-архив все дампы баз данных */
+            # Добавляємо в ZIP-архів всі дампи баз данных
             $n = strlen($dump_dir);
             $n++;
             foreach ($db_files as $file) {
                 $local = substr($file, $n);
                 if (!$zip->addFile($file, $local)) {
-                    echo 'ОШИБКА при добавлении файла '.$file.'<br />';
+                    echo 'ERROR при додаванні файлу '.$file.'<br />';
                 }
             }
             $zip->close();
-            echo '<b><font color="green">СОЗДАН</font></b> архив <b>'.$filezip.'</b><br />';
+            echo '<b><font color="green">СТВОРЕНО</font></b> архив <b>'.$filezip.'</b><br />';
         }
     }
